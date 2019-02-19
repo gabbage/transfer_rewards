a14cc0294df4c2ff2e1a7378e04d4efe73c6eb61
Mohsen Mesgar <mesgar@desktop-k40.ukp.informatik.tu-darmstadt.de>
2019-02-19 16:16:35 +0100

    first commit

---
+++ Refresh/pre-run.sh
@@ -0,0 +1,6 @@
+wget http://kinloch.inf.ed.ac.uk/public/Refresh-NAACL18-1-billion-benchmark-wordembeddings.tar.gz
+wget http://kinloch.inf.ed.ac.uk/public/Refresh-NAACL18-preprocessed-input-data.tar.gz
+wget http://kinloch.inf.ed.ac.uk/public/Refresh-NAACL18-CNN-DM-Filtered-TokenizedSegmented.tar.gz
+wget http://kinloch.inf.ed.ac.uk/public/Refresh-NAACL18-baseline-gold-data.tar.gz
+
+
