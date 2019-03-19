e1d9b31334798920d6be908f898c76e25a54fe4f
Mohsen Mesgar <mohsen.mesgar@h-its.org>
2019-03-19 12:53:43 +0100

    update

--- rl-convAgent/python/train.py
+++ rl-convAgent/python/train.py
@@ -14,6 +14,8 @@ import numpy as np
 import os
 import time
 
+import logging
+
 logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                 datefmt = '%m/%d/%Y %H:%M:%S',
                 level = logging.INFO)
