c3d5ac0a09582bbb036ad39966549e9b0c9ef0d3
Mohsen Mesgar <mohsen.mesgar@h-its.org>
2019-03-20 15:20:02 +0100

    update

--- rl-convAgent/run.sh
+++ rl-convAgent/run.sh
@@ -39,9 +39,9 @@ then
 		exit
 	else
 
-		PATH_TO_MODEL="{$3##/*/}" #basename
-		INPUT_FILE="{$4##/*/}"
-		OUTPUT_FILE="{$5##/*/}"
+		PATH_TO_MODEL=${$3##/*/} #basename
+		INPUT_FILE=${$4##/*/}
+		OUTPUT_FILE=${$5##/*/}
 		log_file='./logs/'$experiment-$mode-$model-path:$PATH_TO_MODEL-inp:$INPUT_FILE-out:$OUTPUT_FILE'.log'
 	fi
 fi
