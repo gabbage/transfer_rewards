23bb8b92a30d899a3352ba8fda99ae70d9c189af
Mohsen Mesgar <mohsen.mesgar@h-its.org>
2019-03-21 15:38:27 +0100

    update

--- rl-convAgent/python/RL/rl_model.py
+++ rl-convAgent/python/RL/rl_model.py
@@ -89,7 +89,8 @@ class PolicyGradient_chatbot():
             pg_loss = pg_loss + pg_current_loss
 
         with tf.variable_scope(tf.get_variable_scope(), reuse=False):
-            train_op = tf.train.AdamOptimizer(self.lr).minimize(pg_loss)
+            #train_op = tf.train.AdamOptimizer(self.lr).minimize(pg_loss)
+            train_op = tf.train.GradientDescentOptimizer(self.lr).minimize(loss)
 
         input_tensors = {
             'word_vectors': word_vectors,
