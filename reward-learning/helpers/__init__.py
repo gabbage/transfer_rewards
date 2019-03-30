096924a006b8146395f6ea9b7ee1d465bbe5ae93
Mohsen Mesgar <mesgar@krusty.ukp.informatik.tu-darmstadt.de>
2019-03-30 10:22:57 +0100

    init

--- reward-learning/helpers/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ["evaluation", "field", "metric_history", "nlp_engine", "vectors", "visdom_plot"]
--- reward-learning/helpers/activations.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-
-class PenalizedTanh(torch.nn.Module):
-    def forward(self, x):
-        return torch.max(torch.tanh(x), 0.25 * torch.tanh(x))
