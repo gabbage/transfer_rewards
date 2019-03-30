d848e69537e7806ec09e1126b4f0ef0551c5342e
Mohsen Mesgar <mohsen.mesgar@h-its.org>
2019-03-30 10:33:42 +0100

    update

--- reward-learning/models/__init__.py
+++
@@ -1,2 +0,0 @@
-__all__ = ['cmpnn', 'mlp_base', 'model_api', 'regression', 'rouge', 'simple_pref', 'simred', 'simred_cnn',
-           'tesauro_symmetric']
--- reward-learning/models/phis/__init__.py
+++
@@ -1,2 +0,0 @@
-__all__ = ['bert', 'bert_simred', 'cnn_encoder', 'embedding', 'encoders', 'infersent', 'mean', 'metrics', 'phi_api',
-           'phi_sequence', 'pmean', 'rnn_encoder', 'sif', 'simred_cnn']
