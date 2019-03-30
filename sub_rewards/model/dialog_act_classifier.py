05c2cc49b4d28d7329ae2037ce960eda7ea054a3
Mohsen Mesgar <mohsen.mesgar@h-its.org>
2019-03-30 13:31:01 +0100

    update

--- sub_rewards/model/dialog_act_classifier.py
+++ sub_rewards/model/dialog_act_classifier.py
@@ -4,31 +4,29 @@ from sklearn.feature_extraction.text import CountVectorizer
 class FeatureBased(object):
 	def __init__(self, train_path, valid_path, test_path):
 		
-		self.train_data = _load(train_path)
+		self.train_path = train_path
 
-		self.valid_path = _load(valid_path)
+		self.valid_path = valid_path
 
-		self.test_path  = _load(test_path)
+		self.test_path  = test_path
 
-	def tokenizeText(self, sample):
-    
-    	tokens = sample.split(' ')
+	def prepare_data(self):
 
-    	tokens = [token.lower().strip() for token in tokens if len(token)>0]
-    
-    	return tokens
+		train_x, train_y = self.load(train_path)
+		train_feat  = text_to_features(train_x)
+		train_label = text_to_label(train_y)
+		self.train = (train_feat, train_label)
+		print(self.train)
 
-	def text_to_features(self, data):
-		'''
-			data: is a list of texts  
-		'''
-		feature_vectors = []
-		# 
+		valid_x, valid_y = self.load(valid_path)
+		valid_feat  = text_to_features(valid_x)
+		valid_label = text_to_label(valid_y) 
+		self.valid = (valid_feat, valid_label)
 
-		# bag of words
-		vectorizer = CountVectorizer(tokenizer=self.tokenizeText, ngram_range=(1,1))
-
-		return feature_vectors
+		test_x, test_y = self.load(test_path)
+		test_feat  	= text_to_features(test_x)
+		test_label 	= text_to_label(test_y) 
+		self.test = (test_feat, test_label)
 
 	def load(self, data_path):
 
@@ -55,3 +53,54 @@ class FeatureBased(object):
 				data_x.append(utt)
 
 		return (data_x, data_y)
+
+	def train(self):
+
+		'''
+		train the model on the training data
+		'''
+
+	def eval(self):
+		'''
+		evaluate the model on the test data
+		'''
+
+	def text_to_label(self, sample):
+
+		return int(sample)
+
+	def tokenizeText(self, sample):
+    
+    	tokens = sample.split(' ')
+
+    	tokens = [token.lower().strip() for token in tokens if len(token)>0]
+    
+    	return tokens
+
+	def text_to_features(self, data_x):
+		'''
+			data: is a list of texts  
+		'''
+		feature_vectors = []
+		# 
+
+		# bag of words
+		vectorizer = CountVectorizer(tokenizer=self.tokenizeText, ngram_range=(1,1))
+
+		feature_vectors = vectorizer(data_x) 
+
+		return feature_vectors
+
+
+
+if __name__== '__main__':
+
+	fb = FeatureBased(train_path= './data/daily_dialog/act_utt.txt',
+								 valid_path='./data/daily_dialog/validation/act_utt.txt',
+								 test_path='./data/daily_dialog/test/act_utt.tx')
+	
+	fb.prepare_data() # convert text data to features
+
+
+
+
