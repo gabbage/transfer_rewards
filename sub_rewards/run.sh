8371ebf6f2656cdb88ccc799f4ca431c5407de29
Mohsen Mesgar <mohsen.mesgar@h-its.org>
2019-03-30 10:53:52 +0100

    update

---
+++ sub_rewards/run.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+###
+##
+# define variables
+machine='gpu' # possible values: {'local', 'gpu'}, where 'local' has no GPU and cuda
+
+env='transfer_reward' 
+
+experiment='test'
+ 
+log_file='./logs/'$experiment
+
+
+
+###
+##
+#check if we are in RL virtual environment
+if [[ $VIRTUAL_ENV != $env ]]
+then
+
+    source activate $env
+
+
+	###
+	##
+	# export the spec of the running environment (rl)
+	conda list > ./documentation/environment-packages-$machine.txt
+
+	conda list --explicit > ./documentation/environment-info-$machine.txt 
+
+	conda env export > ./documentation/environment-$machine.yml
+
+	conda env export --no-builds > ./documentation/environment-nobuilds-$machine.yml
+
+
+fi
+
+
+###
+##
+# run the latest version of the model by getting it from GitHub
+if [[ $machine == "gpu" ]]
+then
+	git checkout logs/*
+	git checkout models/*
+	git pull
+
+fi
+
+
+###
+##
+# Finally execute the model
+
+
