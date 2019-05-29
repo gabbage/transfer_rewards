4ba4607929d33901923b870b779e6079a2184ba9
sebi <sebastian.buecker@stud.tu-darmstadt.de>
2019-05-29 12:27:19 +0200

    glove embedding module

--- sub_rewards/model/embedding.py
+++ sub_rewards/model/embedding.py
@@ -1,16 +1,47 @@
 import random
+import os
 import math
 import numpy as np
+from collections import Counter
 import torch
 import torch.nn as nn
-import torch.nn.init as init
-import torch.nn.functional as F
-from model.attention import Attention
-from torch.nn.modules.distance import CosineSimilarity
+import torchtext as tt
 
 class GloveEmbedding(nn.Module):
-    def __init__(self):
+    def __init__(self, args):
         super(GloveEmbedding, self).__init__()
+        self.args = args
+        self.embed_dim = 300
+        self.vocab = self._load_vocab()
+        self.vocab.load_vectors("glove.42B.300d")
+        self.embed = nn.Embedding(len(self.vocab), self.embed_dim)
+        self.embed.weight.data.copy_(self.vocab.vectors)
 
-    def forward(self, x_left, x_right):
-        pass
+    def _load_vocab(self):
+        f = open(os.path.join(self.args.datadir, "itos.txt"), "r")
+        cnt = Counter()
+        for i, word in enumerate(f):
+            cnt[word[:-1].lower()] = i
+
+        return tt.vocab.Vocab(cnt)
+
+    def forward(self, batch):
+        X = [s[0] for s in batch]
+        X_left = [l for (l,r) in X]
+        X_right = [r for (l,r) in X]
+        print(X_right)
+
+        # to ids
+        X_left = [[[self.vocab.stoi[w] for w in sent] for sent in dialog] for dialog in X_left]
+        X_right = [[[self.vocab.stoi[w] for w in sent] for sent in dialog] for dialog in X_right]
+
+        # to tensors
+        X_left = torch.tensor(X_left, dtype=torch.long)
+        X_right = torch.tensor(X_right, dtype=torch.long)
+
+        # to embedding
+        X_left = self.embed(X_left)
+        X_right = self.embed(X_right)
+
+        return (X_left, X_right)
+
